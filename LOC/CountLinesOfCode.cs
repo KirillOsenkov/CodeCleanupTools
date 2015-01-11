6b85e75a572df1bcc268d7f24cffbfb013bc8ae7
Kirill Osenkov <githubmail@osenkov.com>
2015-01-10 22:19:45 -0800

    Adding LOC - tool to count lines of code.
    cinst loc
    Then just run loc from your solution's root.
    You can also specify a file mask (loc *.js). By default the mask is *.cs.

---
+++ LOC/CountLinesOfCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class CountLinesOfCode
+{
+    static void Main(string[] args)
+    {
+        string pattern = "*.cs";
+        if (args.Length != 0)
+        {
+            pattern = args[0];
+        }
+
+        Console.WriteLine(
+            Directory.GetFiles(
+                Environment.CurrentDirectory,
+                pattern,
+                SearchOption.AllDirectories)
+            .Select(f => File.ReadAllLines(f).Length)
+            .Sum());
+    }
+}
