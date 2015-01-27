12736f39ff3a877a07345c1c18fec06923056363
Kirill Osenkov <kirillo@microsoft.com>
2015-01-26 16:40:50 -0800

    Add FileTypes - tool to list all file extensions in current directory and all subdirectories.

---
+++ FileTypes/FileTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var folder = Environment.CurrentDirectory;
+        var files = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories);
+        HashSet<string> set = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        foreach (var file in files)
+        {
+            var extension = Path.GetExtension(file);
+            set.Add(extension);
+        }
+
+        foreach (var extension in set)
+        {
+            Console.WriteLine(extension);
+        }
+    }
+}
