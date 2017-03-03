4101783941ae2b5acae1faf790bd0d296c818952
Kirill Osenkov <github@osenkov.com>
2017-03-03 15:09:01 -0800

    Adding a Time utility to measure a process' running time.
    Syntax:
    > time process.exe arguments
    
    Equivalent to running
    > process.exe arguments

---
+++ Time/Time.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time
+{
+    public class Time
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine(@"Usage: time <process> <arguments>
+    Prints the duration of a process invocation.");
+            }
+
+            var processFilePath = args[0];
+            string arguments = "";
+            if (args.Length > 1)
+            {
+                arguments = string.Join(" ", args.Skip(1).ToArray());
+            }
+
+            processFilePath = processFilePath.TrimStart('"').TrimEnd('"');
+
+            processFilePath = Path.GetFullPath(processFilePath);
+            if (!File.Exists(processFilePath))
+            {
+                Console.Error.WriteLine($"Application {processFilePath} doesn't exist");
+                return;
+            }
+
+            var processStartInfo = new ProcessStartInfo(processFilePath, arguments);
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = false;
+            processStartInfo.WorkingDirectory = Environment.CurrentDirectory;
+
+            var stopwatch = Stopwatch.StartNew();
+
+            var process = Process.Start(processStartInfo);
+            process.WaitForExit();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(stopwatch.Elapsed.ToString("mm':'ss'.'fff"));
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
