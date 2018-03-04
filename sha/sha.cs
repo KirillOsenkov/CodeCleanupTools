fadfd7c6c9734512281920ff590861e657552135
Kirill Osenkov <github@osenkov.com>
2018-03-04 12:43:24 -0800

    Add a new tool sha to display file sha.

---
+++ sha/sha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+internal class Sha
+{
+    private static void Main(string[] args)
+    {
+        string filePath = null;
+        if (args.Length == 1)
+        {
+            filePath = args[0];
+        }
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File doesn't exist:" + filePath);
+        }
+
+        var hash = Utilities.SHA1Hash(filePath);
+        Console.WriteLine(hash);
+    }
+}
